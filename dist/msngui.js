8a74374e0237cf4ed50bf7a02b8017d56c2213ab
Miroslaw Dylag <miroslaw.dylag@mentorsoftwareltd.com>
2014-12-20 11:51:02 +0100

    Dist

--- dist/msngui.js
+++ dist/msngui.js
@@ -1,6 +1,6 @@
 /**
  * msngui
- * @version v0.0.3 - 2014-12-19
+ * @version v0.0.3 - 2014-12-20
  * @link https://github.com/MentorSoftwareLtd/msNgUi
  * @author Miroslaw Dylag (miroslaw.dylag@mentorsoftwareltd.com)
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -241,7 +241,8 @@ angular.module('ms.NgUi.treeGrid',[])
             //replace: true,
             restrict: 'A',
             scope: {
-                tree: '=ngModel'
+                model: '=ngModel',
+                fields: '=?'
             },
             controller: ["$scope","$compile", "$parse", "$timeout", "$templateCache", function($scope, $compile, $parse, $timeout, $templateCache) {
                 this.$compile = $compile;
@@ -253,20 +254,33 @@ angular.module('ms.NgUi.treeGrid',[])
                 console.log("Ctrl",this, $scope);
             }],
             template : function(templateElement, tAttrs) {
-                console.log('template', templateElement, tAttrs.fields);
+                console.log('template', templateElement.children(), tAttrs);
 
             },
             link: function (scope, elm, attrs, controller) {
-                console.log("Ctrl 2",controller, attrs.fields[0]);
+                console.log("Ctrl 2",controller, attrs);
+                var tBody=angular.element("<tbody></tbody>");
 
+                angular.forEach(scope.fields, function(value,key) {
+                    tBody.append("<th>"+value+"</th>");
+                });
+                elm.append(tBody);
+                angular.forEach(scope.model, function(key,value) {
+                    tBody.append("<tr></tr>");
+                    console.log(value, key);
+
+                });
+
+                //add thead
             }
         };
-    }).directive('treeGridTh', function() {
+    }).directive('treeGridRow', function() {
         return {
-            restrict: 'A',
+            restrict: 'E',
             require: "^treeGrid",
+            template: "<tr></tr>",
             link: function (scope, elm, attrs, controller) {
-                console.log("Ctrl 2",controller);
+                console.log("Treegrid row",scope);
 
             }
         }
